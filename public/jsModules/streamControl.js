9649993bac2145742645dbbb8c9a11185dc33a3b
cfunkles <charleskwilliams16@gmail.com>
2018-07-04 16:26:30 -0600

    rename html file and take out js code into one file

---
+++ public/jsModules/streamControl.js
@@ -0,0 +1,18 @@
+// TODO cancel other song stream when starting new song
+const songButtons = document.getElementsByClassName('playSong');
+console.log(songButtons);
+Array.from(songButtons).forEach(element => {
+    const songName = element.getAttribute('name');
+    element.addEventListener('click',playShape);
+    function playShape() {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+                alert(xmlHttp.responseText);
+                console.log(xmlHttp.responseText);
+            }
+        };
+        xmlHttp.open("GET", 'http://localhost:8080/streamSong/' + songName, true); // true for asynchronous 
+        xmlHttp.send(null);
+    }
+});
