0fbc8b77d305d1ea963879257475adcf1d8c39a6
cfunkles <charleskwilliams16@gmail.com>
2018-07-04 16:28:30 -0600

    stop old stream when running new stream

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // var fs = require('fs');
-let { exec } = require('child_process');
+let { spawn } = require('child_process');
+let process;
 var express = require('express');
 var app = express();
 
@@ -37,14 +38,43 @@ var app = express();
 
 app.get('/streamSong/:song', function(req, res) {
     let song = req.params.song;
-    let cmd = `ffmpeg -re -i ./public/audio/${song}.mp3 -r 10 -vcodec mpeg3 -f mpegts http://localhost:8081/password`;
-    exec(cmd, (err, stdout, stderr) => {
-        if (err) {
-            console.log(err);
-        } else {
-            // TODO show the std out when song is streaming
-            // console.log({ stdout, stderr });
+    if (song === "killStream") {
+        if (process) {
+            process.kill('SIGINT');
+            process = null;
+            console.log('Killing Stream');
+            return res.send('Stream Killed');
         }
+        console.log('No Stream initiated');
+        return res.send('Nothing to Kill');
+    }
+    if (process) {
+        process.kill('SIGINT');
+        console.log('Stopping stream, Starting new stream');
+    }
+    let cmd = 'ffmpeg';
+    let args = [
+        '-re',
+        '-i', `./public/audio/${song}.mp3`,
+        '-r', '10',
+        '-vcodec', 'mpeg3',
+        '-f', 'mpegts',
+        'http://localhost:8081/password'
+    ];
+
+    // Run the child process to play the song
+    process = spawn(cmd, args);
+    // Log the out put
+    process.stdout.on('data',(data) => {
+        console.log(data);
+    });
+    // log the error
+    process.stderr.on('data', (data) => {
+        console.log(data);
+    });
+    // Log when finished
+    process.on('close', () => {
+        console.log('finished');
     });
     res.send('Playing Song');
 });
